b6f8e25016d172e3bcf46464fc42614a56a3e711
Ryan W <ryan_645@outlook.com>
2020-02-12 20:43:13 -0500

    Added connection to database by creatig a query in the home controller

--- Oeuvre/Controllers/HomeController.cs
+++ Oeuvre/Controllers/HomeController.cs
@@ -12,10 +12,12 @@ namespace Oeuvre.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private dbo_OeuvreContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, dbo_OeuvreContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -26,6 +28,37 @@ namespace Oeuvre.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public ViewResult getList()
+        {
+            if (ModelState.IsValid)
+            {
+                //var Galleries = _context.Gallery.Where(p => p.GalleryId == "1");
+
+                var Galleries = _context.Gallery.ToList();               
+
+                List<Gallery> galleryList = new List<Gallery>();
+
+                foreach(Gallery g in Galleries)
+                {
+                    Gallery myGallery = new Gallery();
+
+                    myGallery.GalleryId = g.GalleryId;
+                    myGallery.GalleryName = g.GalleryName;
+                    myGallery.Province = g.Province;
+
+                    galleryList.Add(myGallery);
+                }
+
+                ViewData["Galleries"] = galleryList;
+                //return RedirectToAction("Index");
+                return View("Index");
+            }
+            else
+                return View();
+
+            
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
